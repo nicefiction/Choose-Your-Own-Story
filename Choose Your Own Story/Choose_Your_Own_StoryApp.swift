3dbfe6e20d68947fa4cd323b1aafddedb635b41b
Olivier Van hamme <nicefiction@gmail.com>
2022-04-24 11:48:46 +0200

    Initial Commit

---
+++ Choose Your Own Story/Choose_Your_Own_StoryApp.swift
@@ -0,0 +1,17 @@
+//
+//  Choose_Your_Own_StoryApp.swift
+//  Choose Your Own Story
+//
+//  Created by Olivier Van hamme on 24/04/2022.
+//
+
+import SwiftUI
+
+@main
+struct Choose_Your_Own_StoryApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
